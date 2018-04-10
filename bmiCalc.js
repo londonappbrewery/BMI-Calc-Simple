82d921c0bc8d3029dd2d9dfde54409c4d0648afb
angelabauer <angelabauer@users.noreply.github.com>
2018-04-10 12:33:30 +0100

    Create bmiCalc.js

---
+++ bmiCalc.js
@@ -0,0 +1,12 @@
+//Create your function below this line.
+
+
+
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); 
+
+bmi should equal around 20 in this case.
+
+*/
